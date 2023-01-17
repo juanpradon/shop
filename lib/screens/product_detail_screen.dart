87b6708c43e02a3b3d5908c17dcaaf3a8a7ccddc
juanpradon <juan.f.prado.navea@gmail.com>
2023-01-17 03:06:58 -0300

    cambios

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeName = 'product-detail';
+
+  /*final String title;
+
+  const ProductoDetailScreen({
+    super.key,
+    required this.title,
+  });*/
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
